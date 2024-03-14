064909e28db006c78a360e617b790dfd1952cd15
FedericoPecoraro <federico.pecoraro8@gmail.com>
2024-03-14 21:55:15 +0100

    responsive

---
+++ js/navbar.js
@@ -0,0 +1,24 @@
+let navbar = document.querySelector(".sticky-nav");
+let main = document.querySelector(".main-content");
+let library = document.querySelector(".library");
+let friendBtn = document.querySelector('.users-hp');
+let friendDiv = document.querySelector('.friend');
+
+main.onscroll = function () {
+    let scrolled = this.querySelector(".child-div-sroll").getBoundingClientRect().top;
+    console.log("Hai scrollato di " + scrolled + " pixel.");
+    console.dir(this);
+    if (scrolled > -525) {
+        navbar.style.backgroundColor = "transparent";
+    } else {
+        navbar.style.backgroundColor = "#121212";
+    }
+};
+
+friendBtn.addEventListener('click', function () {
+        friendDiv.classList.toggle('displayB');
+        navbar.classList.toggle('widthNav');
+        
+
+});
+
